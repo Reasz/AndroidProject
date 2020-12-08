ad8cc14a0d53ecaf33ff9496e2edda3db03bddb8
Reasz <73632512+Reasz@users.noreply.github.com>
2020-12-08 15:24:47 +0200

    drawable files added for bottom menu icons activity_main.xml, fragment_profile_screen.xml updated bottom_menu_nav.xml updated colors.xml, strings.xml updated

--- app/src/main/java/com/example/foodproject/MainActivity.kt
+++ app/src/main/java/com/example/foodproject/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.foodproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var bottomNavigationView:BottomNavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            bottomNavigationView!!,
+            navHostFragment!!.navController
+        )
     }
 }
