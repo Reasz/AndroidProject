26cc9813b9227564432b825f10804d9e908234d4
Reasz <73632512+Reasz@users.noreply.github.com>
2020-12-14 09:47:36 +0200

    LoginFragment.kt, RegisterFragment.kt updated, minimal username and password check added nav_graph.xml updated, now you can't go back to the splash screen with the arrows

--- app/src/main/java/com/example/foodproject/frg/LoginFragment.kt
+++ app/src/main/java/com/example/foodproject/frg/LoginFragment.kt
@@ -6,10 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.Navigation
 import com.example.foodproject.R
 
 class LoginFragment : Fragment() {
+
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +28,44 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
 
-        view.findViewById<Button>(R.id.buttonLoginLogin).setOnClickListener{Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_mainScreenFragment2)}
-        view.findViewById<Button>(R.id.buttonLoginRegister).setOnClickListener{Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment2)}
+        initialise(view)
+
+        loginButton.setOnClickListener {
+            val usernameText = usernameEditText.text.toString()
+            val passwordText = passwordEditText.text.toString()
+
+            when {
+                usernameText.isEmpty() -> {
+                    usernameEditText.error = "Username required"
+                }
+                usernameText.length < 3 -> {
+                    usernameEditText.error = "Username must be at least 3 letters"
+                }
+                passwordText.isEmpty() -> {
+                    passwordEditText.error = "Password required"
+                }
+                passwordText.length < 4 -> {
+                    passwordEditText.error = "Password must be at least 4 letters"
+                }
+                else -> {
+                    Navigation.findNavController(view)
+                        .navigate(R.id.action_loginFragment_to_mainScreenFragment2)
+                }
+            }
+        }
+
+        registerButton.setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_loginFragment_to_registerFragment2)
+        }
 
         return view
+    }
+
+    private fun initialise(view: View) {
+        loginButton = view.findViewById(R.id.buttonLoginLogin)
+        registerButton = view.findViewById(R.id.buttonLoginRegister)
+        usernameEditText = view.findViewById(R.id.editTextLoginUsername)
+        passwordEditText = view.findViewById(R.id.editTextLoginPassword)
     }
 }
